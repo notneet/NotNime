95e13520376804e561b9d8b54fa07480f4fb36a1
アニップ <66857547+Hanivan@users.noreply.github.com>
2024-01-17 18:53:11 +0700

    fix: link href mobile version

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -159,7 +159,7 @@ export const Navbar = () => {
                     ? "danger"
                     : "foreground"
                 }
-                href="#"
+                href={item.href}
                 size="lg"
               >
                 {item.label}
